07796c83550fc20da4123981220e5c8fd8f342be
Jean-Baptiste Petit <beanbaptiste.petit@gmail.com>
2016-06-24 19:13:32 +0200

    remove brew
    for the first version I wont use brew

--- osx/main.sh
+++ osx/main.sh
@@ -26,13 +26,13 @@ cp ponyc/build/release/ponyc ponyc-$PONY_VERSION/usr/bin
 cp ponyc/build/release/libponyc.a ponyc-$PONY_VERSION/usr/lib
 cp ponyc/build/release/libponyrt.a ponyc-$PONY_VERSION/usr/lib
 
-fpm -s dir -t brew -n ponyc -v $PONY_VERSION -C ponyc-$PONY_VERSION/
+fpm -s dir -t osxpkg -n ponyc -v $PONY_VERSION -C ponyc-$PONY_VERSION/
 if [[ $? -ne 0 ]]; then
 	echo "Error during the building of Pony"
 	exit 1
 fi
 
-brew install -f ponyc-$PONY_VERSION.tar.gz
+sudo installer -pkg ponyc-$PONY_VERSION.pkg -target /
 if [[ $? -ne 0 ]]; then
 	echo "Error during the building of Pony"
 	exit 1
@@ -44,19 +44,13 @@ if [[ $? -ne 0 ]]; then
 	exit 1
 fi
 
-brew uninstall ponyc-$PONY_VERSION.tar.gz --force
-if [[ $? -ne 0 ]]; then
-	echo "Error during the building of Pony"
-	exit 1
-fi
-
-curl -H "Content-Type: application/json" -u$BINTRAY_USER:$BINTRAY_TOKEN -X POST -d '{"name": "'$PONY_VERSION'","desc": "This is the ponyc nightly"}' https://api.bintray.com/packages/$BINTRAY_USER/brew/ponyc-nightly/versions
+curl -H "Content-Type: application/json" -u$BINTRAY_USER:$BINTRAY_TOKEN -X POST -d '{"name": "'$PONY_VERSION'","desc": "This is the ponyc nightly"}' https://api.bintray.com/packages/$BINTRAY_USER/osx/ponyc-nightly/versions
 if [[ $? -ne 0 ]]; then
 	echo "Error during upload of version"
 	exit 1
 fi
 
-curl -T ponyc-$PONY_VERSION.tar.gz -u$BINTRAY_USER:$BINTRAY_TOKEN -H "X-Bintray-Publish: 1" https://api.bintray.com/content/$BINTRAY_USER/brew/ponyc-nightly/$PONY_VERSION/
+curl -T ponyc-$PONY_VERSION.tar.gz -u$BINTRAY_USER:$BINTRAY_TOKEN -H "X-Bintray-Publish: 1" https://api.bintray.com/content/$BINTRAY_USER/osx/ponyc-nightly/$PONY_VERSION/
 if [[ $? -ne 0 ]]; then
 	echo "Error during upload of file"
 	exit 1
