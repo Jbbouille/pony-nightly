837b8b7e3d2863c147f8809aa3042e798dbc27af
Jean-Baptiste Petit <beanbaptiste.petit@gmail.com>
2016-07-14 19:48:35 +0200

    windows installer

--- windows/on_success.ps1
+++ windows/on_success.ps1
@@ -1,1 +1,9 @@
-#makensis.exe C:\projects\ponyc-nighly\windows\ponyc-installer.nsi
+makensis.exe C:\projects\ponyc-nighly\windows\ponyc-installer.nsi
+
+$securepassword = ConvertTo-SecureString $env:BINTRAY_TOKEN -AsPlainText -Force
+$credentials = New-Object System.Management.Automation.PSCredential($env:BINTRAY_USER, $securepassword)
+$PONY_VERSION = Get-Date -format yyyyMMdd
+
+Invoke-WebRequest -Uri "https://api.bintray.com/packages/$BINTRAY_USER/osx/ponyc-nightly/versions" -Body "{"name": "'$PONY_VERSION'","desc": "This is the ponyc nightly"}" -Credential $credentials -Method Post -ContentType "application/json"
+
+Invoke-WebRequest -Uri "https://api.bintray.com/content/$env:BINTRAY_USER/windows/ponyc-nightly/$PONY_VERSION/" -Credential $credentials -Method Post -InFile "ponyc-installer.exe"
--- windows/test_script.ps1
+++ windows/test_script.ps1
@@ -1,5 +1,5 @@
 C:\projects\ponyc\work\vs2015\..\..\build\release\testc.exe
 C:\projects\ponyc\work\vs2015\..\..\build\release\testrt.exe
-C:\projects\ponyc\work\vs2015\..\..\build\release\ponyc.exe -o C:\projects\ponyc\ -d -s packages/stdlib
+C:\projects\ponyc\work\vs2015\..\..\build\release\ponyc.exe -V 3 -o C:\projects\ponyc\ -d -s packages/stdlib
 stdlib.exe
 del stdlib.exe
