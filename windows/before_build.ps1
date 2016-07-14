161a61af794113418aca60ad74208d458ab4b3d3
Jean-Baptiste Petit <beanbaptiste.petit@gmail.com>
2016-07-14 11:30:17 +0200

    clone to other directory

--- windows/before_build.ps1
+++ windows/before_build.ps1
@@ -1,4 +1,3 @@
-rmdir /s /q C:\projects\ponyc
-git clone -q --depth=10 --branch=master https://github.com/ponylang/ponyc.git C:\projects\ponyc
+git clone -q --depth=5 --branch=master https://github.com/ponylang/ponyc.git C:\projects\ponyc
 cd C:\projects\ponyc
 C:\premake5.exe --with-tests --to=work/vs2015 vs2015
