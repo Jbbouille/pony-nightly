23e5634e52628ddf505e1440b150de8c4e0ba824
Jean-Baptiste Petit <beanbaptiste.petit@gmail.com>
2016-07-14 11:45:45 +0200

    test is not yaml

--- windows/test_script.ps1
+++ windows/test_script.ps1
@@ -1,6 +1,6 @@
-  - C:\projects\ponyc\build\%CONFIGURATION%\testc.exe
-  - C:\projects\ponyc\build\%CONFIGURATION%\testrt.exe
-  - CALL "C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\VC\\bin\\amd64\\vcvars64.bat"
-  - C:\projects\ponyc\build\%CONFIGURATION%\ponyc.exe -V 3 -o C:\projects\ponyc\ -d -s packages/stdlib
-  - stdlib.exe
-  - del stdlib.exe
+C:\projects\ponyc\build\%CONFIGURATION%\testc.exe
+C:\projects\ponyc\build\%CONFIGURATION%\testrt.exe
+CALL "C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\VC\\bin\\amd64\\vcvars64.bat"
+C:\projects\ponyc\build\%CONFIGURATION%\ponyc.exe -V 3 -o C:\projects\ponyc\ -d -s packages/stdlib
+stdlib.exe
+del stdlib.exe
