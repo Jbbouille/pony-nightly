5454a0913f552c9d8730079b5acb0463ec9fcc6e
Jean-Baptiste Petit <beanbaptiste.petit@gmail.com>
2016-07-14 20:10:41 +0200

    go to nsis

--- windows/on_success.ps1
+++ windows/on_success.ps1
@@ -1,4 +1,5 @@
-C:\Program Files (x86)\NSIS\makensis.exe C:\projects\ponyc-nighly\windows\ponyc-installer.nsi
+cd 'C:\Program Files (x86)\NSIS\'
+makensis.exe C:\projects\ponyc-nighly\windows\ponyc-installer.nsi
 
 $securepassword = ConvertTo-SecureString $env:BINTRAY_TOKEN -AsPlainText -Force
 $credentials = New-Object System.Management.Automation.PSCredential($env:BINTRAY_USER, $securepassword)
@@ -7,3 +8,4 @@ $PONY_VERSION = Get-Date -format yyyyMMdd
 Invoke-WebRequest -Uri "https://api.bintray.com/packages/$BINTRAY_USER/osx/ponyc-nightly/versions" -Body "{"name": "'$PONY_VERSION'","desc": "This is the ponyc nightly"}" -Credential $credentials -Method Post -ContentType "application/json"
 
 Invoke-WebRequest -Uri "https://api.bintray.com/content/$env:BINTRAY_USER/windows/ponyc-nightly/$PONY_VERSION/" -Credential $credentials -Method Post -InFile "ponyc-installer.exe"
+
