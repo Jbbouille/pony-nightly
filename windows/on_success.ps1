f37bc6ca392816d3008097126f406f988f4927a5
Jean-Baptiste Petit <beanbaptiste.petit@gmail.com>
2016-07-20 23:39:27 +0200

    send json

--- windows/on_success.ps1
+++ windows/on_success.ps1
@@ -5,6 +5,12 @@ $securepassword = ConvertTo-SecureString $env:BINTRAY_TOKEN -AsPlainText -Force
 $credentials = New-Object System.Management.Automation.PSCredential($env:BINTRAY_USER, $securepassword)
 $PONY_VERSION = Get-Date -format yyyyMMdd
 
-Invoke-WebRequest -Uri "https://api.bintray.com/packages/$BINTRAY_USER/osx/ponyc-nightly/versions" -Body "{"name": "$PONY_VERSION","desc": "This is the ponyc nightly"}" -Credential $credentials -Method Post -ContentType "application/json"
-Invoke-WebRequest -Uri "https://api.bintray.com/content/$env:BINTRAY_USER/windows/ponyc-nightly/$PONY_VERSION/" -Credential $credentials -Method Post -InFile "ponyc-installer.exe"
+$hash = @{  name    = $PONY_VERSION; 
+            desc = "This is the ponyc nightly"
+            }
+
+$JSON = $hash | convertto-json 
+
+Invoke-WebRequest -Uri "https://api.bintray.com/packages/$BINTRAY_USER/osx/ponyc-nightly/versions" -Body $JSON -Credential $credentials -Method Post -ContentType "application/json"
+Invoke-WebRequest -Uri "https://api.bintray.com/content/$env:BINTRAY_USER/windows/ponyc-nightly/$PONY_VERSION/" -Credential $credentials -Method Post -InFile "C:\projects\ponyc-nighly\windows\ponyc-installer.exe"
 
